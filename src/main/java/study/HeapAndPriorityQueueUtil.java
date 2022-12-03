9a29fa6c65518dded7076b6a309be12f2f163fea
aTang <963749001@qq.com>
2022-12-03 16:19:03 +0800

    commit code

---
+++ src/main/java/study/HeapAndPriorityQueueUtil.java
@@ -0,0 +1,30 @@
+package study;
+
+import lombok.Data;
+
+import java.util.Comparator;
+
+public class HeapAndPriorityQueueUtil {
+
+    public int num;
+
+    @Data
+    class Task2 {
+        int priority;
+        String name;
+    }
+
+    // comparator implements
+    private static Comparator<Task2> taskComparable = new Comparator<Task2>() {
+        @Override
+        public int compare(Task2 t1, Task2 t2) {
+            if (t1.getPriority() > t2.getPriority()) {
+                return -1;
+            }else if (t1.getPriority() < t2.getPriority()) {
+                return 1;
+            }
+            return 0;
+        }
+    };
+
+}
